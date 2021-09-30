1683db12858b4b44e1f6508c31a04f5e0ec3d02b
brunobrolesi <brunohsbrolesi@gmail.com>
2021-09-30 19:47:25 -0300

    feat: implements read csv file and generate simple report

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,27 @@
+import csv
+import pathlib
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def __read_file(cls, file_path):
+        with open(file_path) as file:
+            file_extension = pathlib.Path(file_path).suffix
+            if file_extension == '.csv':
+                return list(csv.DictReader(file))
+            if file_extension == '.json':
+                # TODO -> READ JSON FILE
+                return False
+            if file_extension == '.xml':
+                # TODO -> READ XML FILE
+                return False
+
+    @classmethod
+    def import_data(cls, file_path, report_type):
+        data = cls.__read_file(file_path)
+        if report_type == 'simples':
+            return SimpleReport.generate(data)
+        if report_type == 'completo':
+            # TODO -> GENERATE COMPLETE REPORT
+            return False
