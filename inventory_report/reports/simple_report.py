a03ada706613f6b600927549c6756d716d34b792
brunobrolesi <brunohsbrolesi@gmail.com>
2021-09-30 19:40:02 -0300

    refactor: chance @staticmethod to @classmethod and change auxiliary methos to private

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,11 +2,11 @@ from datetime import datetime
 
 
 class SimpleReport:
-    @staticmethod
-    def generate(list):
-        closestValidy = SimpleReport.get_closest_validy(list)
-        olderFabrication = SimpleReport.get_older_fabrication(list)
-        mostStocked = SimpleReport.get_company_with_more_stocked(list)
+    @classmethod
+    def generate(cls, list):
+        closestValidy = cls.__get_closest_validy(list)
+        olderFabrication = cls.__get_older_fabrication(list)
+        mostStocked = cls.__get_company_with_more_stocked(list)
 
         return (
             f"Data de fabricação mais antiga: {olderFabrication}\n"
@@ -15,8 +15,8 @@ class SimpleReport:
             f"estocados: {mostStocked}\n"
         )
 
-    @staticmethod
-    def get_closest_validy(list):
+    @classmethod
+    def __get_closest_validy(cls, list):
         current_date = datetime.now().strftime("%Y-%m-%d")
         dates = [
             product["data_de_validade"]
@@ -26,8 +26,8 @@ class SimpleReport:
         dates.sort()
         return dates[0]
 
-    @staticmethod
-    def get_older_fabrication(list):
+    @classmethod
+    def __get_older_fabrication(cls, list):
         fabrication_dates = [product["data_de_fabricacao"] for product in list]
         fabrication_dates.sort()
         return fabrication_dates[0]
@@ -35,7 +35,7 @@ class SimpleReport:
     # Referência:
     # https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
 
-    @staticmethod
-    def get_company_with_more_stocked(list):
+    @classmethod
+    def __get_company_with_more_stocked(cls, list):
         companies = [product["nome_da_empresa"] for product in list]
         return max(set(companies), key=companies.count)
