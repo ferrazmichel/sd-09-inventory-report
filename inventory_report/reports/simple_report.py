fb0ad1d41ab68040771acca287bfd7cb99fcfedc
Osvaldo Borges <netoo70@gmail.com>
2021-10-01 14:53:55 -0300

    Requisito 01

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+
+def order_date(list_prod):
+    date_order = {}
+    dates = [dates_ant['data_de_fabricacao'] for dates_ant in list_prod]
+    for date in dates:
+        if not date_order:
+            date_order['date_ant'] = date
+        date_new = datetime.strptime(date, '%Y-%m-%d')
+        date_ant = datetime.strptime(date_order['date_ant'], '%Y-%m-%d')
+        if (datetime.now() - date_new) > (datetime.now() - date_ant):
+            date_order['date_ant'] = date
+    return date_order['date_ant']
+
+
+def nearest_date(list_prod):
+    date_nearest = {}
+    dates = [dates['data_de_validade'] for dates in list_prod]
+    for date in dates:
+        if not date_nearest:
+            date_nearest['date_val'] = date
+        date_new = datetime.strptime(date, '%Y-%m-%d')
+        date_val = datetime.strptime(date_nearest['date_val'], '%Y-%m-%d')
+        if date_new < datetime.now():
+            pass
+        elif (date_new - datetime.now()) < (date_val - datetime.now()):
+            date_nearest['date_val'] = date
+    return date_nearest['date_val']
+
+
+def biggest_company(list_prod):
+    companies_big = {}
+    companies = [companies['nome_da_empresa'] for companies in list_prod]
+    for company in companies:
+        if not companies_big.get(company):
+            companies_big[company] = 0
+        companies_big[company] += 1
+    return max(companies_big)
+
+
+class SimpleReport():
+    def generate(list_prod):
+        order = order_date(list_prod)
+        nearest = nearest_date(list_prod)
+        company = biggest_company(list_prod)
+        return (
+            f'Data de fabricação mais antiga: {order}\n'
+            f'Data de validade mais próxima: {nearest}\n'
+            f'Empresa com maior quantidade de produtos estocados: {company}\n'
+        )
