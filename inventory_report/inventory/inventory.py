22d6de5f9a533ccc459251315cf666f971f1cd66
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-10-02 07:35:07 -0300

    req 4 e 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,37 +1,16 @@
-from xml.etree.ElementTree import XMLID
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-
-
-# class CompleteReport(SimpleReport, Manage_list):
-#     """gerador de relatorio - herdada da classe SimpleReport"""
-#     def generate(list):
-#         result_simple_report = SimpleReport.generate(list)
-#         result_manage_list = Manage_list.count_stock(list, 1)
-
-#         """monta a segunda parte da string para retorno"""
-#         companies_rows = ''
-#         for key, value in result_manage_list.items():
-#             companies_rows += '- {}: {}\n'.format(key, value)
-
-#         return (
-#             '{}\n'
-#             'Produtos estocados por empresa: \n'
-#             '{}'
-#             .format(result_simple_report, companies_rows)
-#         )
 
 from functools import lru_cache
 from csv import DictReader
 import json
 from xml.etree import ElementTree
 import os.path
-# path_csv = 'inventory_report/data/inventory.json'
 
 
-# class Inventory(SimpleReport, CompleteReport):
 class Inventory(CompleteReport, SimpleReport):
 
+    """importa e cria uma lista do arquivo csv"""
     @lru_cache
     def import_data_csv(path):
         with open(path, 'r', encoding='utf8') as file:
@@ -39,6 +18,7 @@ class Inventory(CompleteReport, SimpleReport):
             read_list = [rows for rows in spam_reader]
             return read_list
 
+    """importa e cria uma lista do arquivo json"""
     @lru_cache
     def import_data_json(path):
         with open(path) as file:
@@ -46,6 +26,7 @@ class Inventory(CompleteReport, SimpleReport):
             read_list = [rows for rows in json_reader]
             return read_list
 
+    """importa e cria uma lista do arquivo xml"""
     @lru_cache
     def import_data_xml(path):
         tree = ElementTree.parse(path)
@@ -55,10 +36,10 @@ class Inventory(CompleteReport, SimpleReport):
             items = {}
             for subelem in elem:
                 items[subelem.tag] = subelem.text
-                # print(subelem.text)
             read_list.append(items)
         return read_list
 
+    """escolhe o tipo da extenção do arquivo"""
     def import_data(path, option):
         extension = os.path.splitext(path)[1]
 
@@ -75,6 +56,3 @@ class Inventory(CompleteReport, SimpleReport):
             return SimpleReport.generate(list)
         else:
             return CompleteReport.generate(list)
-
-
-# print(Inventory.import_data(path_csv, 'complete'))
