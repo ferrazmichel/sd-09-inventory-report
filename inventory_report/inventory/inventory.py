82cbbe9eed12d4a09ec7c2d175885ce178397866
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-10-04 16:11:49 -0300

    Implementado metodo generico para o Req 3-4-5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,53 @@
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
 from csv import DictReader
 
-from inventory_report.reports.simple_report import SimpleReport
-from inventory_report.reports.complete_report import CompleteReport
+import xml.etree.ElementTree as ET
+import json
+import os
 
 
 class Inventory:
+    def _read_csv(file, extension):
+        if extension != 'csv':
+            raise ValueError("Invalid file extension")
+        else:
+            return list(DictReader(file, delimiter=','))
 
-    def import_data(path, type_report):
-        type = {
+    def _read_json(file, extension):
+        if extension != 'json':
+            raise ValueError("Invalid file extension")
+        else:
+            return json.load(file)
+
+    def _read_xml(path, extension):
+        if extension != 'xml':
+            raise ValueError("Invalid file extension")
+        else:
+            root = ET.parse(path).getroot()
+            read_list = []
+            for element in root:
+                items = {}
+                for subelem in element:
+                    items[subelem.tag] = subelem.text
+                read_list.append(items)
+            return read_list
+
+    @classmethod
+    def import_data(cls, path, type_report):
+        type_generate = {
             "simples": SimpleReport.generate,
             "completo": CompleteReport.generate,
         }
+        extension = path.split(".")[-1]
 
         with open(path, 'r') as file:
-            read_file = list(DictReader(file, delimiter=','))
-            file_report = type[type_report](read_file)
+            type_file = {
+                "csv": cls._read_csv(file, extension),
+                # ".json": cls._read_json(file, extension),
+                # ".xml": cls._read_xml(path, extension),
+            }
+
+            read_file = type_file[extension]
+            file_report = type_generate[type_report](read_file)
             return file_report
