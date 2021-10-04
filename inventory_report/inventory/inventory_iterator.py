a40947cc15dc6eab01cf6c14c7aaf4b2ed2bcaed
Alana Monteiro <lanaah.15@gmail.com>
2021-10-04 10:19:44 -0300

    requisito 7 ok

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,16 +1,16 @@
-class InventoryIterator:
+from _collections_abc import Iterator
+
+
+class InventoryIterator(Iterator):
     def __init__(self, data):
         self.data = data
         self.index = 0
 
     def __next__(self):
-
         try:
             current_value = self.data[self.index]
-
         except IndexError:
             raise StopIteration()
-
         else:
             self.index += 1
             return current_value
--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,49 +1,19 @@
-import sys
-sys.path.append("../")
-
-from reports.complete_report import CompleteReport
-from reports.simple_report import SimpleReport
-from importer.json_importer import JsonImporter
-from importer.csv_importer import CsvImporter
-from importer.xml_importer import XmlImporter
-from inventory_iterator import InventoryIterator
-
-# from ..reports.complete_report import CompleteReport
-# from ..reports.simple_report import SimpleReport
-# from ..importer.json_importer import JsonImporter
-# from ..importer.csv_importer import CsvImporter
-# from ..importer.xml_importer import XmlImporter
-# from .inventory_iterator import InventoryIterator
+from _collections_abc import Iterable
+from .inventory_iterator import InventoryIterator
 
 
-def report_type(type_report, new_data):
-    if type_report == "simples":
-        return SimpleReport.generate(new_data)
-    if type_report == "completo":
-        return CompleteReport.generate(new_data)
-
-
-class InventoryRefactor:
+class InventoryRefactor(Iterable):
     def __init__(self, importer):
-
         self.importer = importer
         self.data = []
 
     def import_data(self, file_path, type_report):
-
         new_data = self.importer.import_data(file_path)
-
-        # result = report_type(type_report, new_data)
-        self.data.append(new_data)
-
-        return self.data
+        if len(new_data):
+            self.data += new_data
+            return
+        self.data.append(new_data[0])
 
     def __iter__(self):
+
         return InventoryIterator(self.data)
-
-
-instance = InventoryRefactor(CsvImporter)
-instance.import_data("../data/inventory.csv", "simples")
-iterator = iter(instance)
-first_item_instance = next(iterator)
-print(first_item_instance)
