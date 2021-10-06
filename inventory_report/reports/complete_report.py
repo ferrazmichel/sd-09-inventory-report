6a1680c217d5646a6961bb17e4ed7dc963bce6e2
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-06 11:34:47 -0300

    apenas criando uma classe herdeira de SimpleReport. Tratar output

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,18 @@
+# [
+#   {
+#     "id": 1,
+#     "nome_do_produto": "Y TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
+#     "nome_da_empresa": "Forces of Nature",
+#     "data_de_fabricacao": "2020-07-04",
+#     "data_de_validade": "2023-02-09",
+#     "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+#     "instrucoes_de_armazenamento": "in intrices    phasellus"
+#   }
+# ]
+
+
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    pass
